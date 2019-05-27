4a47c1f56403467d7dcecdd9bab8ca9bff76993e
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-05-27 11:45:22 -0400

    Init Commit

---
+++ hello_world.py
@@ -0,0 +1,27 @@
+print('Hello World!')
+print('Jon Palacio')
+print(43+2)
+
+city = 'Gastonia'
+price = 2.99
+high_score = 98
+is_having_fun = False
+
+print(f'my city is {city}. a price is {price}. The highscore is {high_score}.  Am I currently having fun? {is_having_fun}')
+
+# data = input("What's your favorite color?")
+# print(f'you said {data}')
+
+# print("whats your favorite color?")
+# data = input()
+# print(f"you said {data}")
+
+print('Whats your name?')
+name = input()
+
+if name == 'Arya Stark':
+    print("Valar Morghulis")
+elif name == "John Snow":
+    print("You know nothing")
+else:
+    print("carry on")
