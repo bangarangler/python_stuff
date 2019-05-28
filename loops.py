e73421ad9d51c5eb37ccc5d4519f7564921bb904
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-05-28 11:51:56 -0400

    Loop python practice

---
+++ loops.py
@@ -0,0 +1,38 @@
+# for letter in 'coffee':
+  # print(letter)
+# for number in range(1,10):
+  # print(number)
+
+# for num in range(0,100,5):
+  # print(num)
+
+# x = 0
+# for n in range(10,21):
+  # if n % 2 != 0:
+    # x += n
+    # print(x)
+
+# times = input("How many times do I have to tell you? ")
+
+# for time in range(times):
+  # print(f"clean up your room: {time+1}")
+
+# for n in range(1,21):
+  # if n == 4 or n == 13:
+    # state = 'unlucky'
+  # elif n % 2 == 0:
+    # state = 'even'
+  # else:
+    # state = 'odd'
+
+# msg = input("whats the secret password?")
+# while msg != 'pirate':
+  # print('wrong!')
+  # msg = input('whats the secret password?')
+# print('correct')
+
+num = 1
+while num < 11:
+  print(num)
+  num += 2
+
