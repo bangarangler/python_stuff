0066ea3443e01e114552702ba0388d28a6fced68
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-05-29 11:54:21 -0400

    practice slices

---
+++ slices.py
@@ -0,0 +1,35 @@
+first_list = [1,2,3,4,5,6]
+
+print(first_list[1:])
+print(first_list[3:])
+print(first_list[-1:])
+print(first_list[-3:])
+print(first_list[:])
+print(first_list[:2])
+print(first_list[:4])
+print(first_list[1:3])
+
+#STEP
+print(first_list[1::2])
+print(first_list[::2])
+print(first_list[1::-1])
+print(first_list[:1:-1])
+print(first_list[2::-1])
+print(first_list)
+
+colors = ['red','orange','yellow','green','blue','indigo','violet']
+
+print(colors[2:])
+print(colors[:])
+print(colors[0:])
+print(colors[-2:])
+print(colors[:5])
+print(colors[2:4])
+print(colors[::2])
+print(colors[::-1])
+print(colors[0])
+print(colors[5][::-1])
+
+string = 'This is fun!'
+print(string[::-1])
+
