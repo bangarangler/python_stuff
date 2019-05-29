a8ab0240d6473cf9574ac3e36e3f87c7ced1bb21
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-05-29 09:02:02 -0400

    more practice

---
+++ for_loops.py
@@ -0,0 +1,14 @@
+numbers = [1,2,3,4]
+colors = ["purple","teal","orange","magenta","crimson","emerald"]
+i = 0
+
+for color in colors:
+  print(color)
+
+# while i < len(numbers):
+  # print(numbers[i])
+  # i += 1
+
+while i < len(colors):
+  print(f"{i}: {colors[i]}")
+  i += 1
