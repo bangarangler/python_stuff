58629eece001cce4a32dfe4aae9a056f15b147f4
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-05-30 08:57:47 -0400

    Nested List Comprehension

--- lists_comprehension.py
+++ lists_comprehension.py
@@ -9,3 +9,21 @@ print([num*2 if num % 2 == 0 else num / 2 for num in numbers])
 
 with_vowels = "This is so much fun!"
 print(''.join(char for char in with_vowels if char not in 'aeiou'))
+
+# Nested Lists
+nested_list = [[1,2,3],[4,5,6],[7,8,9]]
+
+for i in nested_list:
+  for val in i:
+    print(val)
+
+[[print(val) for val in i] for i in nested_list]
+
+coords = [[10.423,9.132],[37.212,-14.092],[21.367,32.572]]
+for loc in coords:
+  for coord in loc:
+    print(coord)
+
+print(['*' for i in range(1,4)])
+
+print([['*' for x in range(1,4)] for i in range(1,4)])
