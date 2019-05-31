bf0a7dfaf312c7d8aee61bcf414c77589c4be2e5
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-05-31 18:14:17 -0400

    Added bunch of Function solutions

--- functions.py
+++ functions.py
@@ -80,4 +80,89 @@ print(speak("duck"))
 print(speak("pig"))
 print(speak("mouse"))
 
+total = 0
+
+def increment():
+  #how to access global .... try to avoid this crazyness
+  global total
+  total += 1
+  return total
+print(increment())
+
+def outer():
+  count = 0
+  def inner():
+    nonlocal count
+    count += 1
+    return count
+  return inner()
+
+def return_day(num):
+  try:
+    return ['Sunday','Monday',"Tuesday","Wednesday","Thursday","Friday","Saturday"][num - 1]
+  except IndexError as e:
+    return None
+
+print(return_day(41))
+
+def last_element(l):
+  if l:
+    return l[-1]
+  return None
+
+def number_compare(a,b):
+  if a > b:
+    return "First is greater"
+  elif b > a:
+    return "Second is greater
+  return "Numbers are equal
+
+def single_letter_count(string, letter):
+  return string.lower().count(letter.lower())
+
+def multiple_letter_count(string):
+  return {letter: string.count(letter) for letter in string}
+
+def list_manipulation(collection, command, location, value=None):
+  if (command == 'remove' and location == "end"):
+    return collection.pop()
+  elif (command == 'remove' and location == 'beginning'):
+    return collection.pop(0)
+  elif (command == 'add' and location == 'beginning'):
+    collection.insert(0,value)
+    return collection
+  elif (command == 'add' and location == 'end'):
+    collection.append(value)
+    return collection
+
+  # def is_palindrome(string):
+    # return string == string[::-1]
+
+  def is_palindrome(string):
+    stripped = string.replace(" ", "")
+    return stripped == stripped[::-1]
+
+  def frequency(collection, searchTerm):
+    return collection.count(searchTerm)
+
+  def multiply_even_numbers(lst):
+    total = 1
+    for val in lst:
+      if val % 2 == 0:
+        total = total * val
+    return total
+
+  def capitalize(string):
+    return string[:1].upper() + string[1:]
+
+  def compact(l):
+    return [val for val in l if val]
+
+  def intersection(l1,l2):
+    return [val for val in l1 if val in l2]
+
+  def partition(lst, fn):
+    return [[val for val in lst if fn(val)], [val for val in lst if not
+                                              fn(val)]]
+
 
