9dd4596d03441647de13fed90e1f2448ff8c8360
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-06-01 11:47:23 -0400

    intro pdb

---
+++ debugging.py
@@ -0,0 +1,18 @@
+# import pdb
+first = "First"
+second = "Second"
+# pdb.set_trace()
+result = first + second
+third = "Third"
+result += third
+print(result)
+
+#l,n,c, type vars to see values l list, n next, c continue
+
+def add_numbers(a,b,c,d):
+  import pdb; pdb.set_trace()
+
+  return a + b + c + d
+add_numbers(1,2,3,4)
+
+
