d9cfa6831f699a9fcc1402c5f0456eda0055e099
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-06-01 10:53:18 -0400

    zip

--- functions2.py
+++ functions2.py
@@ -52,3 +52,32 @@ print(abs(-5))
 
 #sum takes iterable and optional start returns sum plus the start
 print(sum([1,2,3], 10))
+print(sum([1,2,3]))
+
+# round(3.4567)
+
+def max_magnitude(nums):
+  return max(abs(num) for num in nums)
+
+def sum_even_values(*args):
+  return sum(arg for arg in args if arg % 2 == 0)
+
+def sum_floats(*args):
+  return sum(arg for arg in args if type(arg) == float)
+
+#ZIP make iterator that aggregates elements from each iterable.
+
+nums1 = [1,2,3,4,5]
+nums2 = [6,7,8,9,10]
+
+z = zip(nums1, nums2)
+print(list(z))
+
+def interleave(str1, str2):
+  return ''.join(' '.join(x) for x in (zip(str1,str2)))
+
+def triple_and_filter(lst):
+  return list(filter(lambda x: x % 4 == 0, map(lambda x: x*3, lst)))
+
+def extract_full_name(l):
+  return list(map(lambda val: "{} {}".format(val['first'], val['last']), l))
