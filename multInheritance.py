f11c3e0496acff30c0b66e17e701a52e6ef16f28
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-06-06 20:06:51 -0400

    Multiple Inheritance.... just to know you can

---
+++ multInheritance.py
@@ -0,0 +1,36 @@
+class Aquatic:
+  def __init__(self,name):
+    print("AQUATIC INIT!")
+    self.name = name
+
+  def swim(self):
+    return f"{self.name} is swimming"
+
+  def greet(self):
+    return f"I am {self.name} of the sea!"
+
+class Ambulatory:
+  def __init__(self,name):
+    print("AMBULATORY INIT!")
+    self.name = name
+
+  def walk(self):
+     return f"{self.name} is walking"
+
+  def greet(self):
+     return f"I am {self.name} of the land!"
+
+class Penguin(Ambulatory, Aquatic):
+  def __init__(self,name):
+    print("PENGUIN INIT!")
+    # super().__init__(name=name)
+    Ambulatory.__init__(self,name=name)
+    Aquatic.__init__(self,name=name)
+
+babyshark = Aquatic("Baby B Shark")
+fido = Ambulatory("Fido Rover")
+pin = Penguin("Captain Pin")
+
+print(pin.swim())
+print(pin.walk())
+print(pin.greet())
