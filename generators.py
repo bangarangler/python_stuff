f6118de052053e4ae2e5f7929af64d64a46601c8
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-06-07 12:29:11 -0400

    Save Memory ... generator typically
    better / easier on generator

--- generators.py
+++ generators.py
@@ -28,3 +28,17 @@ def current_beat():
     if i >= len(nums): i = 0
     yield nums[i]
     i += 1
+
+counter = current_beat()
+
+def fib_gen(max):
+  x = 0
+  y = 1
+  count = 0
+  while count < max:
+    x,y = y, x + y
+    yield x
+    count += 1
+
+for n in fib_gen(10):
+  print(n)
