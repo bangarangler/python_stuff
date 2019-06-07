78ab422178ba8a7f5b5460948bff02b9e650aff2
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-06-07 12:21:17 -0400

    Generators continued...

--- generators.py
+++ generators.py
@@ -19,3 +19,12 @@ counter = count_up_to(10)
 print(next(counter))
 for num in counter:
   print(num)
+
+# BEAT INFINITE GENERATOR
+def current_beat():
+  nums = (1,2,3,4)
+  i = 0
+  while True:
+    if i >= len(nums): i = 0
+    yield nums[i]
+    i += 1
