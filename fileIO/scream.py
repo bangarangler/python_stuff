1486f833d97c99baa9d2a5607cd0129b55e13551
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-06-09 15:22:16 -0400

    Update and modify data from csv file to a new file

---
+++ fileIO/scream.py
@@ -0,0 +1,13 @@
+from csv import reader, writer
+with open("fighters.csv") as file:
+  csv_reader = reader(file)
+  # fighters = [[s.upper() for s in row] for row in  csv_reader]
+  with open('screaming_fighters.csv', "w") as file:
+    csv_writer = writer(file)
+    for fighter in csv_reader:
+      csv_writer.writerow([s.upper() for s in fighter])
+
+# with open("screaming_fighters.csv", "w") as file:
+  # csv_writer = writer(file)
+  # for fighter in fighters:
+    # csv_writer.writerow(fighter)
